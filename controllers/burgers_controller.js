ba44344e961c35259cee69bc0f15f97734f6e13e
Charli <charlispilecki@gmail.com>
2021-01-23 00:33:03 -0600

    Create back end controller and views

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,31 +2,40 @@ var express = require('express')
 var path = require("path");
 var burgersBook = require('../models/burgers_book')
 
-var app = express();
+var router = express.Router()
 
-pp.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
+router.get("/", function(req, res) {
+    burgersBook.getBooks(function(result) {
+        var unreadBooks = result.filter(function (book) {
+            return book.book_read === 0
+        })
+        var readBooks = result.filter(function (book) {
+            return book.book_read === 1
+        })
+        res.render('index', {
+            unreadBooks: unreadBooks,
+            readBooks: readBooks
+        })
+    })
 });
 
-app.get("/api/burgers_books", function(req, res) {
+router.get("/api/burgers_books", function(req, res) {
     burgersBook.getBooks(function (result) {
         res.json(result)
     });
 });
 
-app.post("/api/burgers_books", function(req, res) {
-    burgersBook.addBook(req.body.bookName, function (result) {
+router.post("/api/burgers_books", function(req, res) {
+    console.log(req.body)
+    burgersBook.addBook(req.body.book_name, function (result) {
         res.json(result)
     });
 });
 
-app.put("/api/burgers_books/:id", function(req, res) {
-    burgersBook.updateBook(req.params.id, req.body.bookRead, function (result) {
+router.put("/api/burgers_books/:id", function(req, res) {
+    burgersBook.updateBook(req.params.id, req.body.book_read, function (result) {
         res.json(result)
     });
 });
 
-
+module.exports = router
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-let express = require('express')
+var express = require('express')
+var burgersControler = require('./controllers/burgers_controller')
+var exphbs  = require('express-handlebars');
+ 
+var app = express();
+ 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// Sets up the Express app to handle css and js files
+app.use(express.static('public'))
 
+app.use('/', burgersControler)
 
+app.listen(8080)
