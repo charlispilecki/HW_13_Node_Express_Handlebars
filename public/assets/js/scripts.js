c2e47d2c118cf00bded69c7bb7fa3a6e18213b80
Charli <charlispilecki@gmail.com>
2021-01-23 00:33:17 -0600

    Add event listeners in front end

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,33 @@
+function addBook() {
+    var newBookName = document.querySelector('#new').value
+    var newBook = {
+        book_name: newBookName,
+        book_read: 0
+    }
+    fetch('api/burgers_books', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newBook)
+    }).then(response => {
+        location.reload()
+    })
+}
+
+function readBook(id) {
+    fetch('api/burgers_books/' + id, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            book_read: 1
+        })
+    }).then(response => {
+        location.reload()
+    })
+}
+
+document.querySelector('#add').addEventListener('click', addBook)
+
