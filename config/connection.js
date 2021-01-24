077044c337dab554a7bbc805f270aead3015457a
Charli <charlispilecki@gmail.com>
2021-01-23 19:18:10 -0600

    Configure database connection to work in Heroku

--- config/connection.js
+++ config/connection.js
@@ -4,16 +4,22 @@
 
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
+var con 
+if (process.env.JAWSDB_URL) {
+  con = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  con = mysql.createConnection({
     host: "localhost",
     database: "burgers_booklist_db",
     user: "root",
     password: "griffin1"
   });
-  
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
+}
 
-  module.exports = con; 
+  
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+module.exports = con; 
